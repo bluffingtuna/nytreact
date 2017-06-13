05eb978566d09b7837291603adaaacc06e073c63
bluffingtuna@gmail.com <bluffingtuna@gmail.com>
2017-06-12 22:51:08 -0700

    duplicates validation added

--- models/Articles.js
+++ models/Articles.js
@@ -1,12 +1,18 @@
 var mongoose = require("mongoose");
+let beautifyUnique = require('mongoose-beautiful-unique-validation');
+
 
 var Schema = mongoose.Schema;
 
 var articleSchema = new Schema({
-	title: String,
-	url: String
-},
-{timestamps: true})
+    title: {
+        type: String,
+        unique: true
+    },
+    url: String
+}, { timestamps: true })
+
+articleSchema.plugin(beautifyUnique);
 
 var Article = mongoose.model("Article", articleSchema);
 
